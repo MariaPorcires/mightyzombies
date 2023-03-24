aaeaddb9cc8d8dbc62ac039e9e51f25fad17a3eb
Lellisinthewoods <ellinor.romin@hotmail.com>
2023-03-24 13:53:28 +0100

    added css to menuitem

--- airbean/src/components/menuitem/MenuITEM.jsx
+++ airbean/src/components/menuitem/MenuITEM.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { useState } from 'react';
+import './MenuITEM.css'
 
 
 function MenuITEM(props) {
-    const {title} = props.coffee;
+    const {title, desc, price} = props.coffee;
 
     function handleClick(){
         console.log(title)
     }
 
     return (
-        <div onClick={handleClick}>{title}</div>
+        <article className='menuitem' onClick={handleClick}>
+            <h2 className='menuitem__title'>{title}</h2>
+            <h3 className='menuitem__desc'>{desc}</h3>
+            <h2 className='menuitem__title menuitem__price'>{price} kr</h2>
+        </article>
     )
 }
 

1009561d2cd79e432011763009e1d5a0c8532de0
hellnomary <maria.porcires@outlook.com>
2023-03-24 09:42:04 +0100

    deleted ellinors dumb stuff

--- airbean/src/App.jsx
+++ airbean/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useDispatch } from 'react-redux'
-import { increase, decrease } from './actions/counterActions'
-import { ViewCounter } from './viewCounter/viewCounter'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">

b681938e87e06d0c456dc0c904f4b14a393cca11
Adrianaxj <35222944+Adrianaxj@users.noreply.github.com>
2023-03-28 10:15:24 +0200

    cat

--- airbean/src/pages/shoppingCart_page/shoppingCart_page.jsx
+++ airbean/src/pages/shoppingCart_page/shoppingCart_page.jsx
@@ -4,6 +4,10 @@ function ShoppingCart_page() {
   return (
     <div>ShoppingCart_page</div>
   )
+  async function saveOrder(order) {
+    const response = await fetch('https://airbean.awesomo.dev/api/beans/order', { method: 'POST', body: order} );
+    const data = await response.json();
+}
 }
 
 export default ShoppingCart_page

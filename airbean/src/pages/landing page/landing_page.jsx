c4f26d12579264641b370a29ddaca9c4f4887c91
hellnomary <maria.porcires@outlook.com>
2023-04-03 17:14:56 +0200

    landing

--- airbean/src/pages/landing page/landing_page.jsx
+++ airbean/src/pages/landing page/landing_page.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './landing_page.css'
+import { useNavigate } from 'react-router';
 
 function Landing_page() {
+  const navigate = useNavigate();
+
+    function navMenu() {
+      navigate('/')
+    }
+
   return (
-    <section className="landing_page">
-    <img className='landing-slider__left' src="../src/assets/assets/graphics/intro-graphic-left.svg" /> 
+    <section className="landing_page" onClick={ navMenu }>
+    <img className='landing__left' src="../src/assets/assets/graphics/intro-graphic-left.svg" /> 
     <img className='landing-__logo' src="../src/assets/assets/graphics/airbean-landing.svg" />
-    <img className='landing-slider__right' src="src/assets/assets/graphics/intro-graphic-right.svg" />
+    <img className='landing__right' src="src/assets/assets/graphics/intro-graphic-right.svg" />
     
     </section>
 
